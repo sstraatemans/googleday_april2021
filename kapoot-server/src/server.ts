2ff4de5c1e6101597270eec88c477039d246cdc3
Jan-Willem Baart <jwbaart@gmail.com>
2021-04-16 12:22:37 +0200

    build: server port

--- kapoot-server/src/server.ts
+++ kapoot-server/src/server.ts
@@ -1,9 +1,10 @@
 import * as express from "express";
 import * as socketio from "socket.io";
 import * as path from "path";
+import { Socket } from "socket.io";
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8080);
 
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
@@ -12,23 +13,42 @@ app.get("/", (req: any, res: any) => {
   res.sendFile(path.resolve("./index.html"));
 });
 
+let participants: string[] = [];
+
 // simple '/' endpoint sending a Hello World
 // response
 app.get("/hello-world", (req: any, res: any) => {
   res.send("hello world");
 });
 
-io.on("connection", function(socket: any) {
-  console.log("a user connected");
+io.use((socket: any, next: any) => {
+  const username = socket.handshake.auth.username;
+  console.log('username middleware', username)
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
 
-  socket.on("message", function(message: any) {
-    console.log(message);
-
-    socket.emit("message", message + "<-- your message")
+io.on("connection", (socket: Socket) => {
+  socket.onAny((event, ...args) => {
+    console.log(event, args);
   });
+
+  socket.on("RegisterParticipant", (participantName: string) => {
+    participants.push(participantName);
+    // TODO: filter double names
+    io.emit('PartipantsUpdated', { participants })
+  })
+
+  socket.on("GameStarted", () => {
+      console.log('Game has started');
+      io.emit('NewQuestion', { participants })
+  })
 });
 
 // start our simple server up on localhost:3000
-const server = http.listen(3000, function() {
+const server = http.listen(3000, function () {
   console.log("listening on *:3000");
 });
