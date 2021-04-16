0295efa1aecfbd3f30d1c41b235e701ba633db20
Steven Straatemans <steven@straatemans.nl>
2021-04-16 15:45:02 +0200

    giving answer

--- kapoot/components/QuestionController/index.tsx
+++ kapoot/components/QuestionController/index.tsx
@@ -1,16 +1,46 @@
 import React, { FC, useState } from 'react';
-import { NewQuestionSocketEvent, Question } from '../../../types/sockets.types';
+import { useSocket } from 'use-socketio';
+import {
+  GiveAnswerSocketEvent,
+  NewQuestionSocketEvent,
+  Question,
+} from '../../../types/sockets.types';
+import { socketEmit } from '../../utils/socketEmit';
 import { useLastMessage } from '../../utils/useLastMessage';
 
 interface IQuestion {}
 
 const QuestionController: FC = () => {
   const [question, setQuestion] = useState<Question>();
+  const [answerGiven, setAnswerGiven] = useState<string | undefined>();
+  const { socket } = useSocket();
+
   useLastMessage<NewQuestionSocketEvent>('NewQuestion', (data) => {
-    //setQuestion(data.response);
+    setQuestion(data.value);
   });
 
-  return <div>(GAME STARTED!) question controller {JSON.stringify(question)}</div>;
+  const answerQuestion = (id: string) => {
+    setAnswerGiven(id);
+
+    socketEmit<GiveAnswerSocketEvent>(socket, 'GiveAnswer', { answerId: answerGiven });
+  };
+
+  if (!question) return <div>waiting for question</div>;
+  return (
+    <div>
+      <h1>{question.question}</h1>
+
+      {question.answers.map((answer) => (
+        <button
+          style={{ backgroundColor: answer.id === answerGiven ? 'green' : 'grey' }}
+          type='button'
+          onClick={() => answerQuestion(answer.id)}
+        >
+          {answer.displayValue}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionController;
