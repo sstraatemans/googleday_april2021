08ce850d39f0cf801ebaf8ec388d015071ad159e
Angelo Michel <angelo.michel@frontmen.nl>
2021-04-16 14:03:25 +0200

    socket io added in register

--- kapoot/components/RegisterParticipant/index.tsx
+++ kapoot/components/RegisterParticipant/index.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useSocket } from "use-socketio";
 import { PartipantRegisteredSocketEvent } from "../../../types/sockets.types";
 import { useGameContextConsumer } from "../../context/GameContext";
 import { useLastMessage } from "../../context/MockSocket";
 
 const RegisterParticipant = () => {
   const [error, setError] = useState(null);
+  const inputRef = useRef<HTMLInputElement>();
   const { setState } = useGameContextConsumer();
+  const { socket } = useSocket();
 
   const {
     data: participantRegisteredResponse,
@@ -14,11 +17,13 @@ const RegisterParticipant = () => {
   useEffect(() => {
     if (participantRegisteredResponse) {
       if (participantRegisteredResponse.success) {
+        console.log("PartipantRegistered->success");
         setState({
           registered: true,
           name: "asd",
         });
       } else {
+        console.log("PartipantRegistered->failed");
         setError("Register failed, choose a different name!");
       }
     }
@@ -26,13 +31,18 @@ const RegisterParticipant = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const name = inputRef.current.value;
 
-    // RegisterParticipantSocketEvent { name }
+    if (!name) {
+      inputRef.current.focus();
+    } else {
+      socket.emit("RegisterParticipant", { name });
+    }
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <input />
+      <input ref={inputRef} />
       {error}
       <button>Register</button>
     </form>
