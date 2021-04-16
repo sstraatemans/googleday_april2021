9c8d8ab434c4ce1d46753963f440d9643a9748f8
Steven Straatemans <steven@straatemans.nl>
2021-04-16 13:35:43 +0200

    testing socketio

--- kapoot-server/src/server.ts
+++ kapoot-server/src/server.ts
@@ -1,59 +1,64 @@
-import * as express from "express";
-import * as socketio from "socket.io";
-import * as path from "path";
-import { Socket } from "socket.io";
+import * as express from 'express';
+import * as socketio from 'socket.io';
+import * as path from 'path';
+import { Socket } from 'socket.io';
 
 const app = express();
-app.set("port", process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 
-let http = require("http").Server(app);
-let io = require("socket.io")(http);
+let http = require('http').Server(app);
+let io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
-app.get("/", (req: any, res: any) => {
-  res.sendFile(path.resolve("./index.html"));
+app.get('/', (req: any, res: any) => {
+  res.sendFile(path.resolve('./index.html'));
 });
 
 let participants: string[] = [];
 
 // simple '/' endpoint sending a Hello World
 // response
-app.get("/hello-world", (req: any, res: any) => {
-  res.send("hello world");
+app.get('/hello-world', (req: any, res: any) => {
+  res.send('hello world');
 });
 
 io.use((socket: any, next: any) => {
   const name = socket.handshake.auth.name;
-  console.log('name middleware', name)
+  console.log('name middleware', name);
   if (!name) {
-    return next(new Error("invalid name"));
+    return next(new Error('invalid name'));
   }
   socket.name = name;
   next();
 });
 
-io.on("connection", (socket: Socket) => {
+io.on('connection', (socket: Socket) => {
   socket.onAny((event, ...args) => {
     console.log(event, args);
   });
 
-  socket.on("RegisterParticipant", (participantName: string) => {
-      let success: boolean = false;
-      if (participants.indexOf(participantName) !== -1) {
-        participants.push(participantName);
-        io.emit('PartipantsUpdated', { participants })
-      }
-    
-      socket.emit('PartipantRegistered', {
-        success,
-    })
-  })
+  socket.on('RegisterParticipant', (participantName: string) => {
+    let success: boolean = false;
+    if (participants.indexOf(participantName) !== -1) {
+      participants.push(participantName);
+      io.emit('PartipantsUpdated', { participants });
+    }
 
-  socket.on("GameStarted", () => {
-      console.log('Game has started');
-      io.emit('NewQuestion', { participants })
-  })
+    socket.emit('PartipantRegistered', {
+      success,
+    });
+  });
+
+  socket.on('GameStarted', () => {
+    console.log('Game has started');
+    io.emit('NewQuestion', { participants });
+  });
 });
 
 const server = http.listen(8080, function () {
-  console.log("listening on *:8080");
+  console.log('listening on *:8080');
 });
--- kapoot/pages/index.js
+++ kapoot/pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Choose a game</h1>
-        <a href="/hoofdsteden-van-de-wereld">Hoofdsteden</a>
+        <a href='/hoofdsteden-van-de-wereld'>Hoofdsteden</a>
       </main>
     </div>
-  )
+  );
 }
