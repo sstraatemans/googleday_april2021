1f4dc894c105b1420232843d72f579c5074e9970
Jan-Willem Baart <jwbaart@gmail.com>
2021-04-16 11:15:14 +0200

    fix: basic events

--- kapoot-server/src/server.ts
+++ kapoot-server/src/server.ts
@@ -1,14 +1,31 @@
 import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-var http = require("http").Server(app);
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/", (req: any, res: any) => {
+  res.sendFile(path.resolve("./index.html"));
+});
 
 // simple '/' endpoint sending a Hello World
 // response
-app.get("/", (req: any, res: any) => {
+app.get("/hello-world", (req: any, res: any) => {
   res.send("hello world");
+});
+
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+
+  socket.on("message", function(message: any) {
+    console.log(message);
+
+    socket.emit("message", message + "<-- your message")
+  });
 });
 
 // start our simple server up on localhost:3000
