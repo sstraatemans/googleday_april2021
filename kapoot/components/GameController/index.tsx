5942b714621416ba1270c70ab0395ee0dc133692
Angelo Michel <angelo.michel@frontmen.nl>
2021-04-16 11:20:48 +0200

    socket mock

--- kapoot/components/GameController/index.tsx
+++ kapoot/components/GameController/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useGameContextConsumer } from "../../context/GameContext";
+import {
+  ParticipantsUpdatedSocketEvent,
+  useLastMessage,
+} from "../../context/MockSocket";
 import RegisterParticipant from "../RegisterParticipant";
 
 const GameController = () => {
   const { state } = useGameContextConsumer();
+  const { participants } = useLastMessage<ParticipantsUpdatedSocketEvent>(
+    "ParticipantsUpdated"
+  );
+
   if (!state.registered) {
     return <RegisterParticipant />;
   }
 
-  return <div>let the game begin!</div>;
+  return <div>let the game begin! {JSON.stringify(participants)}</div>;
 };
 
 export default GameController;
